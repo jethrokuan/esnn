9c71e8a28f264a5b2ef93b6374f70d839a891210
Jethro Kuan <jethrokuan95@gmail.com>
2020-01-07 20:26:13 +0800

    Add more datasets

---
+++ esnn/demos/iris.py
@@ -0,0 +1,19 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from esnn.esnn import ESNN
+from esnn.encoder import Encoder
+
+X, y = load_iris(return_X_y = True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+encoder = Encoder(20, 1.5, 0, 8)
+esnn = ESNN(encoder,
+            m=0.9, c=0.7, s=0.6)
+esnn.train(X_train, y_train)
+y_pred = esnn.test(X_test)
+
+acc = accuracy_score(y_test, y_pred)
+
+print(f"Accuracy: {len(esnn.all_neurons)}")
+print(f"Accuracy: {acc}")
--- esnn/demos/mnist.py
+++ esnn/demos/mnist.py
@@ -1,18 +1,19 @@
-from sklearn.datasets import load_iris
+from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from esnn import ESNN
-from encoder import Encoder
+from esnn.esnn import ESNN
+from esnn.encoder import Encoder
 
-X, y = load_iris(return_X_y = True)
+X, y = load_digits(return_X_y = True)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-encoder = Encoder(20, 1.5, 0, 8)
+encoder = Encoder(10, 0.9, 0, 255)
 esnn = ESNN(encoder,
-            m=0.9, c=0.7, s=0.6)
+            m=0.9, c=0.6, s=0.6)
 esnn.train(X_train, y_train)
 y_pred = esnn.test(X_test)
 
 acc = accuracy_score(y_test, y_pred)
 
+print(f"Accuracy: {len(esnn.all_neurons)}")
 print(f"Accuracy: {acc}")
